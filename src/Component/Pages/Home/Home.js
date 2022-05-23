1c3d0dc486970b62728f03b0d3351cd90f58fc4b
unknown <simeonolawale69@gmail.com>
2022-05-23 16:29:21 +0100

    charts

--- src/Component/Pages/Home/Home.js
+++ src/Component/Pages/Home/Home.js
@@ -1,40 +1,44 @@
-import React from 'react'
+import React from "react";
 //import Navbar from '../../Navbar/Navbar'
-import './sass/home.css'
-import { AiOutlineArrowDown } from "react-icons/ai";
-const Home= () => {
+import "./sass/home.css";
+import Recharts from '../Recharts/Recharts'
+import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
+const Home = () => {
   return (
-    <div className='Home'>
-        <h2>
-    
-        Overview
-        </h2>
-      <div className='rate'>
-        <div className='mainBox'>
-        <h4>Total Profit</h4>
-        <div className='Rate'>
-        <span>$95,595 </span>
-          <span>-3.55%  <AiOutlineArrowDown/></span>
+    <div className="Home">
+      <h2>Overview</h2>
+      <div className="rate">
+        <div className="mainBox">
+          <h4>Total Profit</h4>
+          <div className="Rate">
+            <span >$95,595 </span>
+            <span className="up">
+              +3.55%  <AiOutlineArrowUp/>
+            </span>
+          </div>
         </div>
+        <div className="mainBox">
+          <h4>Total Expensis</h4>
+          <div className="Rate">
+            <span>$95,595 </span>
+            <span className="up">
+              +3.67% <AiOutlineArrowUp/>
+            </span>
+          </div>
         </div>
-        <div className='mainBox'>
-        <h4>Total Profit</h4>
-        <div className='Rate'>
-        <span>$95,595 </span>
-          <span>-3.55%  <AiOutlineArrowDown/></span>
-        </div> 
+        <div className="mainBox">
+          <h4>New Users</h4>
+          <div className="Rate">
+            <span>$95,595 </span>
+            <span className="down">
+              -3.55% <AiOutlineArrowDown />
+            </span>
+          </div>
         </div>
-        <div className='mainBox'>
-        <h4>Total Profit</h4>
-        <div className='Rate'>
-        <span>$95,595 </span>
-          <span>-3.55%  <AiOutlineArrowDown/></span>
-        </div>
-        </div>
-        </div>
-        </div>
-    
-  )
-}
+      </div>
+      <Recharts/>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
