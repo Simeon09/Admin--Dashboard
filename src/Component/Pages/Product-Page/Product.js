6d27d39a6b1da32aa31823655c430ef10460cf14
unknown <simeonolawale69@gmail.com>
2022-05-27 16:34:15 +0100

    client page done

--- src/Component/Pages/Product-Page/Product.js
+++ src/Component/Pages/Product-Page/Product.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
+import "./sass/product.css";
 const Product = () => {
+  const [products, setproducts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakerapi.it/api/v1/products?_quantity=6")
+      .then((response) => response.json())
+      .then((data) => setproducts(data.data));
+  }, []);
   return (
-    <div> <p>this d product page</p></div>
-  )
-}
+    <>
+      <div className=" product">
+        {products
+          ? products?.map((product, idx) => (
+              <div className=" productsCards">
+                <div className="productImage">
+                  <img src={product.image} alt="icon" />
+                </div>
+                <div className="productDeescription">
+                  <h4> {product.name}</h4>
+                  <p> Net: ${product.net_price}</p>
+                  <p> Taxe: {product.taxes}</p>
+                  <p> Price: ${product.price}</p>
+                </div>
+              </div>
+            ))
+          : null}
+      </div>
+    </>
+  );
+};
 
-export default Product
+export default Product;
