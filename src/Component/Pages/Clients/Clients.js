0e244eb3be3013c2aa68abceee5c79b1d860b1b2
unknown <simeonolawale69@gmail.com>
2022-05-27 15:15:52 +0100

    client page done

--- src/Component/Pages/Clients/Clients.js
+++ src/Component/Pages/Clients/Clients.js
@@ -4,13 +4,14 @@ import Avatar from "../../images/avatar.png";
 const Clients = () => {
   const [clients, setclients] = useState([]);
   useEffect(() => {
-    fetch("https://fakerapi.it/api/v1/companies?_quantity=1")
+    fetch("https://fakerapi.it/api/v1/companies?_quantity=7")
       .then((response) => response.json())
       .then((data) => setclients(data.data));
   }, []);
-
+        console.log(clients)
   return (
     <div className="client">
+      <h1> Welcome to the Client Page</h1>
       <table>
         <thead>
           <tr>
@@ -25,14 +26,18 @@ const Clients = () => {
           {clients
             ? clients?.map((client, idx) => (
                 <tr key={idx}>
-                  <td>{client.name}</td>
+                  <td>{client.name}
+                  <p> <span> {client.firstname}</span>
+                      <span> {client.lastname}</span>
+                      </p>
+                  </td>
 
                   <td> {client.email}</td>
                   <td>{client.phone}</td>
-                  <td> {client.gender}</td>
+                  <td> {client.contact.gender}</td>
                   <td>
-                    {" "}
-                   {client.image} 
+                    <img src={client.image} alt='icon'/>
+                   
                   </td>
                 </tr>
               ))
