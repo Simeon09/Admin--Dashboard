fc56a50ae3346265c2f693018e70fb5117eb5e30
unknown <simeonolawale69@gmail.com>
2022-05-30 22:50:46 +0100

    rechart

--- src/Component/Pages/Recharts/Recharts.js
+++ src/Component/Pages/Recharts/Recharts.js
@@ -100,7 +100,7 @@ const Recharts = () => {
           //  width={"100%"}
           // height={"100%"}
             data={data}
-            margin={{ top: 5, right: 0, left: 0, bottom: 0 }}
+            margin={{ top: 5, right: 10, left: 0, bottom: 0 }}
           >
             <defs>
               <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
